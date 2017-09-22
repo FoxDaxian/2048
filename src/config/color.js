eb2c5dc8ca960e6bf837b1591776ac4267ea4f3a
fox <a13821190779>
2017-09-22 11:20:13 +0800

    :sparkles: 基本功能完成，需要优化代码，补动画效果

---
+++ src/config/color.js
@@ -0,0 +1,20 @@
+const colors = {
+	'2' : '#EFE3DC',
+	'4' : '#EDE1CB',
+	'8' : '#F5B17C',
+	'16' : '#F69566',
+	'32' : '#F97C61',
+	'64' : '#F95D3E',
+	'128' : '#EED179',
+	'256' : '#EECE69',
+	'512' : '#98CF31',
+	'1024' : '#2CB4E4',
+	'2048' : '#0098CA',
+	'4096' : '#AB62C9',
+	'8192' : '#9A26C8',
+	boxBg: '#CDC1B4',
+	bg: '#BBADA1',
+	fontColor: '#fff'
+}
+
+export default colors
--- src/javascript/game.js
+++ src/javascript/game.js
@@ -1,4 +1,5 @@
 import Canvas from './Canvas.js'
+import colors from '../config/color.js'
 
 // 游戏规则很简单：每次控制所有方块向同一个方向运动，两个相同数字的方块撞在一起之后合并成为他们的和，每次操作之后会在空白的方格处随机生成一个2或者4，最终得到一个“2048”的方块就算胜利了。如果16个格子全部填满并且相邻的格子都不相同也就是无法移动的话，那么恭喜你，gameover。
 
@@ -18,26 +19,17 @@ class Index extends Canvas{
 		this.initrandArr()
 
 		this.init()
-		this.operate()
+		this.bindEvent()
 
 	}
 
 	// 游戏入口
 	init () {
-		this.ctx.clearRect(0, 0, this.size, this.size)
-		this.ctx.fillStyle = '#BBADA0'
-		this.ctx.fillRect(0, 0, this.size, this.size)
-		this.drawBg()
-
-		const rand = this.randArr.splice(Math.ceil(Math.random() * this.randArr.length) - 1, 1)
-		const x = rand % this.count
-		const y = ~~(rand / this.count)
-		this.state[x][y] = 2
-
-		this.drawBox(x, y, 2)
+		this.preOperate()
+		this.randProdBox()
 	}
 
-	// 初始化状态
+	// 初始化二位数组状态
 	initState () {
 		for (let i = 0; i++ < this.count;) {
 			this.state.push([])
@@ -52,45 +44,43 @@ class Index extends Canvas{
 		}
 	}
 
-	// 画方块背景
+	// 初始化和重绘之前的操作
+	preOperate () {
+		this.ctx.clearRect(0, 0, this.size, this.size)
+		this.drawBg()
+		this.drawBoxBg()
+	}
+
+	// 画大背景
 	drawBg () {
-		this.ctx.fillStyle = '#CDC1B4'
-		this.ctx.fillRect(0 + this.boxGap, this.boxGap, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize + this.boxGap * 2, this.boxGap, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 2 + this.boxGap * 3, this.boxGap, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 3 + this.boxGap * 4, this.boxGap, this.boxSize, this.boxSize)
+		this.ctx.fillStyle = colors.bg
+		this.ctx.fillRect(0, 0, this.size, this.size)
+	}
 
-		this.ctx.fillRect(0 + this.boxGap, this.boxGap * 2 + this.boxSize, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize + this.boxGap * 2, this.boxGap * 2 + this.boxSize, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 2 + this.boxGap * 3, this.boxGap * 2 + this.boxSize, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 3 + this.boxGap * 4, this.boxGap * 2 + this.boxSize, this.boxSize, this.boxSize)
-
-		this.ctx.fillRect(0 + this.boxGap, this.boxGap * 3 + this.boxSize * 2, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize + this.boxGap * 2, this.boxGap * 3 + this.boxSize * 2, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 2 + this.boxGap * 3, this.boxGap * 3 + this.boxSize * 2, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 3 + this.boxGap * 4, this.boxGap * 3 + this.boxSize * 2, this.boxSize, this.boxSize)
-
-		this.ctx.fillRect(0 + this.boxGap, this.boxGap * 4 + this.boxSize * 3, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize + this.boxGap * 2, this.boxGap * 4 + this.boxSize * 3, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 2 + this.boxGap * 3, this.boxGap * 4 + this.boxSize * 3, this.boxSize, this.boxSize)
-		this.ctx.fillRect(this.boxSize * 3 + this.boxGap * 4, this.boxGap * 4 + this.boxSize * 3, this.boxSize, this.boxSize)
+	// 画方块槽
+	drawBoxBg () {
+		this.ctx.fillStyle = colors.boxBg
+		for (let i = 0; i < this.count; i++) {
+			for (let j = 0; j < this.count; j++) {
+				this.ctx.fillRect(this.boxSize * (j + 0) + this.boxGap * (j + 1), this.boxGap * (i + 1) + this.boxSize * i, this.boxSize, this.boxSize)
+				this.ctx.fillRect(this.boxSize * (j + 1) + this.boxGap * (j + 2), this.boxGap * (i + 1) + this.boxSize * i, this.boxSize, this.boxSize)
+				this.ctx.fillRect(this.boxSize * (j + 2) + this.boxGap * (j + 3), this.boxGap * (i + 1) + this.boxSize * i, this.boxSize, this.boxSize)
+				this.ctx.fillRect(this.boxSize * (j + 3) + this.boxGap * (j + 4), this.boxGap * (i + 1) + this.boxSize * i, this.boxSize, this.boxSize)
+			}
+		}
 	}
 
 	// 画一个随机的方块
 	drawBox (x, y, num) {
-		// X 坐标 i : 0 -> 3
-		// this.ctx.fillRect((w * i) + this.boxGap * (i + 1), this.boxGap, w, w)
-		// Y 坐标 j : 0 -> 3
-		// this.ctx.fillRect(this.boxGap, (w * j) + this.boxGap * (j + 1), w, w)
 
 		const realX = (this.boxSize * x) + this.boxGap * (x + 1)
 		const realY = (this.boxSize * y) + this.boxGap * (y + 1)
 
-		this.ctx.fillStyle = '#EEE4DA'
+		this.ctx.fillStyle = colors[num]
 		this.ctx.fillRect(realX, realY, this.boxSize, this.boxSize)
 
 		this.ctx.save()
-		this.ctx.fillStyle = 'red'
+		this.ctx.fillStyle = colors.fontColor
 		this.ctx.textAlign = 'center'
 		this.ctx.textBaseline = 'middle'
 		this.ctx.font = '30px sans-serif'
@@ -98,7 +88,7 @@ class Index extends Canvas{
 		this.ctx.restore()
 	}
 
-	// 画全部的方块
+	// 画操作之后的所有方块，对应state
 	drawAll () {
 		for (var x = 0; x < this.count; x++) {
 			for (var y = 0; y < this.count; y++) {
@@ -109,15 +99,26 @@ class Index extends Canvas{
 		}
 	}
 
+	// 随机生成一个方块坐标
+	randProdBox () {
+		const rand = this.randArr.splice(Math.ceil(Math.random() * this.randArr.length) - 1, 1)
+		if (rand.length) {
+			const num = Math.random() > 0.5 ? 2 : 4
+			const x = rand % this.count
+			const y = ~~(rand / this.count)
+			this.state[x][y] = num
+			this.drawBox(x, y, num)
+		}
+	}
+
 	// 操作并计算重绘所有方块
-	leftOperate (dir) {
-		if (this.canMove(dir)) {
+	operate (dir) {
+		if (this.hasMove(dir)) {
 			this.justMove(dir)
 		}
 		this.drawAll()
 	}
 
-	// 左右移动还是有问题，弄完左右弄上下，一步步来吧，碎了
 	// 进行移动操作
 	justMove (dir) {
 		const fn = (x, y, tempx, tempy) => {
@@ -125,110 +126,51 @@ class Index extends Canvas{
 				this.state[x][y] = this.state[tempx][tempy]
 				this.state[tempx][tempy] = undefined
 
+				
 				this.delFromRandArr(y * this.count + x, this.randArr)
 				this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-				if (dir === 1) {
-					x--
-				}
-				if (dir === 2) {
-					x++
-				}
-				if (dir === 3) {
-					y++
-				}
-				if (dir === 4) {
-					y--
-				}
+				return true
 			} else {
 				if (this.state[x][y] === this.state[tempx][tempy]) {
 					this.state[x][y] *= 2
 					this.state[tempx][tempy] = undefined
 
 					this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-					if (dir === 1) {
-						x--
-					}
-					if (dir === 2) {
-						x++
-					}
-					if (dir === 3) {
-						y++
-					}
-					if (dir === 4) {
-						y--
-					}
+					// 这个true不能要，不然会造成：4 2 2 -> 8 
+					// 正确的是：4 2 2 -> 4 4
+					// return true
 				} else {
 					if (dir === 1 && x + 1 !== tempx) {
+						// 左
 						this.state[x + 1][y] = this.state[tempx][tempy]
 						this.state[tempx][tempy] = undefined
 						this.delFromRandArr(y * this.count + x + 1, this.randArr)
 						this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-						if (dir === 1) {
-							x--
-						}
-						if (dir === 2) {
-							x++
-						}
-						if (dir === 3) {
-							y++
-						}
-						if (dir === 4) {
-							y--
-						}
+						return true
 					}
 					if (dir === 2 && x - 1 !== tempx) {
+						// 右
 						this.state[x - 1][y] = this.state[tempx][tempy]
 						this.state[tempx][tempy] = undefined
 						this.delFromRandArr(y * this.count + x - 1, this.randArr)
 						this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-						if (dir === 1) {
-							x--
-						}
-						if (dir === 2) {
-							x++
-						}
-						if (dir === 3) {
-							y++
-						}
-						if (dir === 4) {
-							y--
-						}
+						return true
 					}
-					if (dir === 3 && y + 1 !== tempx) {
+					if (dir === 3 && y + 1 !== tempy) {
+						// 上
 						this.state[x][y + 1] = this.state[tempx][tempy]
 						this.state[tempx][tempy] = undefined
 						this.delFromRandArr((y + 1) * this.count + x, this.randArr)
 						this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-						if (dir === 1) {
-							x--
-						}
-						if (dir === 2) {
-							x++
-						}
-						if (dir === 3) {
-							y++
-						}
-						if (dir === 4) {
-							y--
-						}
+						return true
 					}
-					if (dir === 4 && y + 1 !== tempx) {
-						this.state[x][y + 1] = this.state[tempx][tempy]
+					if (dir === 4 && y - 1 !== tempy) {
+						// 下
+						this.state[x][y - 1] = this.state[tempx][tempy]
 						this.state[tempx][tempy] = undefined
-						this.delFromRandArr((y + 1) * this.count + x, this.randArr)
+						this.delFromRandArr((y - 1) * this.count + x, this.randArr)
 						this.insetToRandArr(tempy * this.count + tempx, this.randArr)
-						if (dir === 1) {
-							x--
-						}
-						if (dir === 2) {
-							x++
-						}
-						if (dir === 3) {
-							y++
-						}
-						if (dir === 4) {
-							y--
-						}
+						return true
 					}
 				}
 			}
@@ -238,12 +180,24 @@ class Index extends Canvas{
 		if (dir === 1) {
 			for (let y = 0; y < this.count; y++) {
 				for (let x = 0; x < this.count; x++) {
-					// 一个比较重要的点：因为randarr的长度改变了，所以出现bug
 					let condition = this.findExist(x, y, dir)
 
 					if (condition !== -1) {
 						const {x: tempx, y: tempy} = condition
-						fn(x, y, tempx, tempy)
+						if (fn(x, y, tempx, tempy)) {
+							if (dir === 1) {
+								x--
+							}
+							if (dir === 2) {
+								x++
+							}
+							if (dir === 3) {
+								y--
+							}
+							if (dir === 4) {
+								y++
+							}
+						}
 					}
 				}
 			}
@@ -252,12 +206,24 @@ class Index extends Canvas{
 		if (dir === 2) {
 			for (let y = 0; y < this.count; y++) {
 				for (let x = this.count - 1; x > -1; x--) {
-					// 一个比较重要的点：因为randarr的长度改变了，所以出现bug
 					let condition = this.findExist(x, y, dir)
 
 					if (condition !== -1) {
 						const {x: tempx, y: tempy} = condition
-						fn(x, y, tempx, tempy)
+						if (fn(x, y, tempx, tempy)) {
+							if (dir === 1) {
+								x--
+							}
+							if (dir === 2) {
+								x++
+							}
+							if (dir === 3) {
+								y--
+							}
+							if (dir === 4) {
+								y++
+							}
+						}
 					}
 				}
 			}
@@ -266,13 +232,24 @@ class Index extends Canvas{
 		if (dir === 3) {
 			for (let x = 0; x < this.count; x++) {
 				for (let y = 0; y < this.count; y++) {
-					// 一个比较重要的点：因为randarr的长度改变了，所以出现bug
 					let condition = this.findExist(x, y, dir)
-					console.log(condition)
 
 					if (condition !== -1) {
 						const {x: tempx, y: tempy} = condition
-						fn(x, y, tempx, tempy)
+						if (fn(x, y, tempx, tempy)) {
+							if (dir === 1) {
+								x--
+							}
+							if (dir === 2) {
+								x++
+							}
+							if (dir === 3) {
+								y--
+							}
+							if (dir === 4) {
+								y++
+							}
+						}
 					}
 				}
 			}
@@ -281,12 +258,24 @@ class Index extends Canvas{
 		if (dir === 4) {
 			for (let x = 0; x < this.count; x++) {
 				for (let y = this.count - 1; y > -1; y--) {
-					// 一个比较重要的点：因为randarr的长度改变了，所以出现bug
 					let condition = this.findExist(x, y, dir)
 
 					if (condition !== -1) {
 						const {x: tempx, y: tempy} = condition
-						fn(x, y, tempx, tempy)
+						if (fn(x, y, tempx, tempy)) {
+							if (dir === 1) {
+								x--
+							}
+							if (dir === 2) {
+								x++
+							}
+							if (dir === 3) {
+								y--
+							}
+							if (dir === 4) {
+								y++
+							}
+						}
 					}
 				}
 			}
@@ -309,7 +298,7 @@ class Index extends Canvas{
 		arr.splice(i, 1)
 	}
 
-	// 找不为undefined的,这个时候向左移动的时候用的
+	// 找任意一排中存在的值
 	findExist (x, y, dir) {
 		// 左
 		if (dir === 1) {
@@ -358,7 +347,7 @@ class Index extends Canvas{
 			if (y === 0) {
 				return -1
 			}
-			for (let i = y + 1; i < this.count; i++) {
+			for (let i = y - 1; i > - 1; i--) {
 				if (typeof this.state[x][i] !== 'undefined') {
 					return {
 						y: i,
@@ -370,8 +359,8 @@ class Index extends Canvas{
 		return -1
 	}
 
-	// 能不能移动
-	canMove (dir) {
+	// 有没有可以移动的方块
+	hasMove (dir) {
 		return this.state.some((el, i) => {
 			for (let j = 0; j < this.count; j++) {
 				// 左
@@ -406,51 +395,41 @@ class Index extends Canvas{
 	// 处理键盘事件
 	keyboardHandle (e) {
 		const ev = e || window.event
+		if (ev.keyCode === 37 || ev.keyCode === 38 || ev.keyCode === 39 || ev.keyCode === 40) {
+			this.preOperate()
 
+			/** 方向对应num
+			**  左：1
+			**  右：2
+			**  上：3
+			**	下：4
+			**/
+			switch (ev.keyCode) {
+				case 37:
+				// 左
+				this.operate(1)
+				break;
+				case 39:
+				// 右
+				this.operate(2)
+				break;
+				case 38:
+				// 上
+				this.operate(3)
+				break;
+				case 40:
+				// 下
+				this.operate(4)
+				break;
+			}
 
-		this.ctx.clearRect(0, 0, this.size, this.size)
-		this.ctx.fillStyle = '#BBADA0'
-		this.ctx.fillRect(0, 0, this.size, this.size)
-		this.drawBg()
-
-
-		/** 方向对应num
-		**  左：1
-		**  右：2
-		**  上：3
-		**	下：4
-		**/
-		switch (ev.keyCode) {
-			case 37:
-			// console.log('左')
-			this.leftOperate(1)
-			break;
-			case 39:
-			// console.log('右')
-			this.leftOperate(2)
-			break;
-			case 38:
-			// console.log('上')
-			this.leftOperate(3)
-			break;
-			case 40:
-			// console.log('下')
-			this.leftOperate(4)
-			break;
-		}
-
-
-		// 再随机生成一个
-		const rand = this.randArr.splice(Math.ceil(Math.random() * this.randArr.length) - 1, 1)
-		if (rand.length) {
-			const x = rand % this.count
-			const y = ~~(rand / this.count)
-			this.state[x][y] = 2
-			this.drawBox(x, y, 2)
+			// 再随机生成一个
+			this.randProdBox()
 		}
 	}
 
-	operate () {
+	// 绑定键盘事件
+	bindEvent () {
 		canvas.addEventListener('keydown', this.keyboardHandle.bind(this))
 	}
 }
